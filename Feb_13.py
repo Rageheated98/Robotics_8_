59123602225f16f4ef8c39ac36fcbb48cf9870b6
Rageheated98 <prithvim185@stemcollegiate.ca>
2025-02-13 10:10:27 -0700

    Create Feb_13.py

---
+++ Feb_13.py
@@ -0,0 +1,6 @@
+# i leared that you could print for all of them to work
+print("Mathew)
+print("Well we")
+print("just use more lines")
+print("of code")
+print("""Anything that starts with three quotes,
